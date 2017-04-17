842b51c416b31635b22c1299bca14d19466b1684
Kai Zhu <Kevin.zhu.cn@outlook.com>
2017-04-16 23:48:27 -0500

    4.16.1

--- ddl/createTable.java
+++ ddl/createTable.java
@@ -1,5 +1,12 @@
 package ddl;
 
 public class createTable {
-
+	//check if exist
+	public void create(){
+		
+	}
+	
+	public void log(Object msg){
+		System.out.println(String.valueOf(msg));
+	}
 }
--- ddl/showTables.java
+++ ddl/showTables.java
@@ -1,5 +1,11 @@
 package ddl;
 
 public class showTables {
-
+	public void show(){
+		
+	}
+	
+	public void log(Object msg){
+		System.out.println(String.valueOf(msg));
+	}
 }
